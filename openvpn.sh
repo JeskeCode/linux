7bc346875165829ee51d56e82068894aaa4d3b2f
0xFFF0007 <JeskeCode@users.noreply.github.com>
2014-10-23 11:59:59 -0400

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -1,2 +1,8 @@
 apt-get install network-manager-openvpn network-manager-pptp \
         network-manager-pptp-gnome network-manager-vpnc
+        
+wget https://www.privateinternetaccess.com/openvpn/openvpn.zip
+
+mv openvpn.zip /etc/openvpn
+cd /etc/openvpn
+unzip openvpn.zip
