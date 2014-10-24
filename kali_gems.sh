f7f6c5f1169685f22cb7b8f51a509e47bdf5b24b
0xFFF0007 <JeskeCode@users.noreply.github.com>
2014-10-23 21:07:17 -0400

    Update kali_gems.sh

--- kali_gems.sh
+++ kali_gems.sh
@@ -1,8 +1,8 @@
-# Feeling a little more adventurous? Want to build the latest version of Kali? 
-# Want to customize your ISO? Looking for KDE, LXDE, MATE, XFCE and other customizations? 
-# This is the option for you. With everything set up correctly, the basic process is as simple as
-apt-get install git live-build cdebootstrap
-git clone git://git.kali.org/live-build-config.git
-cd live-build-config
-lb config
-lb build
+
+
+apt-get remove iceweasel
+echo -e "\ndeb http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main" | tee -a /etc/apt/sources.list > /dev/null
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C1289A29
+apt-get update
+apt-get install firefox-mozilla-build
+
