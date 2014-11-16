9a381e78084f1cd77d83da1dee219d02dcaba6a2
0xFFF0007 <JeskeCode@users.noreply.github.com>
2014-11-15 20:34:21 -0500

    Delete openvpn.sh

--- openvpn.sh
+++
@@ -1,8 +0,0 @@
-apt-get install network-manager-openvpn network-manager-pptp \
-        network-manager-pptp-gnome network-manager-vpnc
-        
-wget https://www.privateinternetaccess.com/openvpn/openvpn.zip
-
-mv openvpn.zip /etc/openvpn
-cd /etc/openvpn
-unzip openvpn.zip
