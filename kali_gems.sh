2be89e5dd10f6aaf84a216eef38dbd7d5e9df9b0
0xFFF0007 <JeskeCode@users.noreply.github.com>
2014-11-20 22:31:44 -0500

    Update kali_gems.sh

--- kali_gems.sh
+++ kali_gems.sh
@@ -1,12 +1,3 @@
 
-#install firefox
-apt-get remove iceweasel
-echo -e "\ndeb http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main" | tee -a /etc/apt/sources.list > /dev/null
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C1289A29
-apt-get update
-apt-get install firefox-mozilla-build
-
-#install python easy tools
-apt-get install python-setuptools
-easy_install pip
+#install 
 
