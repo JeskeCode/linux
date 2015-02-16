60854de316eb52f886b449d039c9a6aa01bbbfe0
0xFFF0007 <JeskeCode@users.noreply.github.com>
2015-02-16 08:51:19 -0500

    Update kali_gems.sh

--- kali_gems.sh
+++ kali_gems.sh
@@ -1,3 +1,16 @@
 
-#install 
+# install on virutal box
 
+vi /etc/apt/sources.list 
+deb http://http.kali.org/kali kali main non-free contrib
+deb-src http://http.kali.org/ kali kali main non-free contrib
+deb http://security.kali.org/kali-security kali/updates main contrib non-free
+
+apt-get update && apt-get install -y linux-headers-$(uname -r) 
+
+cd media
+ls -l VBoxLinuxAdditions.run
+cp VBoxLinuxAdditions.run /root
+chmod 755 VBoxLinuxAdditions.run
+ls -l  VBoxLinuxAdditions.run
+./VBoxLinuxAdditions.run
